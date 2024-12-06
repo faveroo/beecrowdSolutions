80260ce3c6536f92407d281c69a95865eeb3f52b
Favero <74825744+faveroo@users.noreply.github.com>
2024-12-06 09:51:08 -0300

    Create beecrowd1006.lua

---
+++ beecrowd/beecrowd1006.lua
@@ -0,0 +1,5 @@
+local a = tonumber(io.read())
+local b = tonumber(io.read())
+local c = tonumber(io.read())
+local media = ((a * 2) + (b * 3) + (c * 5)) / 10
+print(string.format("MEDIA = %.1f", media))
